5c39d24a0e2a386818ef1f9f17feb890e1f3afc7
jerin-kunjumon <jkjerryomalloor@gmail.com>
2024-04-23 09:20:41 +0530

    Cookie generation

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from  'dotenv';
+import cookieParser from "cookie-parser";
 import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js"
 import UserRoutes from './routes/userRoutes.js'
@@ -10,6 +11,11 @@ const port = process.env.PORT || 5000 ;
 
 
 connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+app.use(cookieParser())
 app.use('/api/users', UserRoutes)
 
 app.get('/',(req,res)=>res.send('server is ready'));
