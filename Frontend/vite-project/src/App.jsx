67c5e56fa5145e161676e8d0b17d052e680cf7ee
jerin-kunjumon <jkjerryomalloor@gmail.com>
2024-05-16 09:57:47 +0530

    router setup done

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header'
-
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { Container } from 'react-bootstrap';
+const App = () => {
   return (
-    <>
-      <div>
-        <Header />
-        <h2>Mern app</h2>
-      </div>
-    </>
+    <div>
+      <Header />
+      <Container className='my-2'>
+      <Outlet />
+        </Container>
+
+
+    </div>
   )
 }
 
